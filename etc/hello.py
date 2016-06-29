5386cdbbcff082ebcbefa00de5bd272a0588e2fd
Mars <marsart@yandex.ru>
2016-06-29 15:17:18 +0300

    work with database

---
+++ etc/hello.py
@@ -0,0 +1,14 @@
+pythonpath = '/home/box/web/'
+bind = "0.0.0.0:8080"
+
+
+
+'''CONFIG = {
+    'working_dir': '/home/box/web',
+    'args': (
+        '--bind=0.0.0.0:8080',
+        '--workers=16',
+        '--timeout=60',
+        'application.hello',
+    ),
+}'''
